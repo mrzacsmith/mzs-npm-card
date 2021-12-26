f9390dec2f6fd9dc9ce93a0218c9ba0a52bb31f4
Zac Smith <zrs3141592@gmail.com>
2021-12-26 11:40:09 -0700

    update contact information

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const info = {
-  Name: 'Zac Smith',
-  Brand: 'Mr Zac Smith',
-  NPMjs: 'https://www.npmjs.com/~mrzacsmith',
-  GitHub: 'https://github.com/mrzacsmith',
-  LinkedIn: 'https://www.linkedin.com/in/mrzacsmith/',
-  Twitter: 'https://twitter.com/mrzacsmith',
-  YouTube: 'https://www.youtube.com/c/zrs3141592',
-  Email: 'zac@mrzacsmith.com',
-}
+const clearConsole = require('clear-any-console')
+clearConsole()
 
-console.log('')
-for (const data in info) {
-  console.log(`* ${data}: \t${info[data]}`)
-}
-console.log('')
+console.log(`
+Zac Smith - MrZacSmith
+
+Node engineer that researchs secure and efficient microserivce back-ends using Node.js, Express.js, MongoDb, and Google Cloud.
+
+🐦 Twitter:  https://twitter.com/mrzacsmith
+💻 GitHub:   https://github.com/mrzacsmith
+🎱 NPMjs:    https://www.npmjs.com/~mrzacsmith 
+🧐 LinkedIn: https://www.linkedin.com/in/mrzacsmith/
+🎬 YouTube:  https://www.youtube.com/c/CodeShockDev
+📧 Email:    zac@codeshock.dev
+
+`)
